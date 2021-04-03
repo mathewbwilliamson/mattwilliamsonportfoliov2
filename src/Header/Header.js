48c712cfe4cf245a334f38041e9f6d84223ffaff
Matt Williamson <mathewbwilliamson@gmail.com>
2021-04-03 15:12:03 -0400

    Updated basic CSS Styles for main page

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { SiLinkedin, SiGithub } from 'react-icons/si';
+
 import './Header.css';
 
 export const Header = () => {
@@ -9,8 +11,8 @@ return (
         <div className='link-container'>
             <Link to='/work'>Work</Link>
             <Link to='/contact'>Contact</Link>
-            <span>LinkedIn</span>
-            <span>Github</span>
+            <a href='https://www.linkedin.com/in/matt-b-williamson/'><SiLinkedin size={'1.5rem'} /></a>
+            <a href='https://github.com/mathewbwilliamson'><SiGithub size={'1.5rem'} /></a>
         </div>
     </div>
     );
