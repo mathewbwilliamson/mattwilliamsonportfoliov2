53b0f1609957a4a6e84db7d365ed3fd76c8180f8
Matt Williamson <mathewbwilliamson@gmail.com>
2021-04-04 16:03:39 -0400

    Updated based on resume updates

--- src/WorkPage/WorkPage.js
+++ src/WorkPage/WorkPage.js
@@ -9,12 +9,12 @@ return (
         <p><span className="word-highlight">Relevant Tech:</span> Typescript, React, Node, Express, Koa, SQL, C#, .Net Core, HTML/CSS</p>
         <p>
             <ul>
-                <li>Worked on a team of four people, that later grew into eight people, supporting <span className="word-highlight">4 products for Bank OZK</span>, including building two entirely new products.</li>
-                <li>Two of the four products support the bank's large commercial real estate lending unit. The commercial loan construction deals that flow through these two products make up <span className="word-highlight">65% of the bank's total revenue</span>. One product is a complete asset management solution for these loans, and the other is a checklist application to ensure every loan closes with the documents that it requires.</li>
+                <li>Collaborated on a team of four people that later grew into eight people, <span className="word-highlight">supporting 4 products for Bank OZK</span>, including building two entirely new products.</li>
+                <li>Two of the four products support the bank's large commercial real estate lending unit. The commercial loan construction deals that flow through these two products make up 65% of the bank's total revenue. One product is a complete asset management solution for these loans, and the other is a checklist application to ensure every loan closes with the documents that it requires.</li>
                 <li>Helped lead the effort to <span className="word-highlight">reduce production incidents 50%</span> in one year.</li>
-                <li>Part of a team of four that made a new Approval System product (I did all the initial setup work). This product has every Bank OZK real estate loan coming through it, and the C-Suite uses it weekly to approve all large +20MM$ loans through the bank. We built this in a <span className="word-highlight">speedy 4 months</span> with React/OvermindJS on the front end, with a C# .Net backend from start to finish.</li>
-                <li>Took over another bank project and built a new <span className="word-highlight">proof of concept in a record 1 week</span> to enable my team lead to make a case for our team to take over this new project.</li>
-                <li>Lately, I have been tasked to build a new Commercial Lending platform with a team of four others. <span className="word-highlight">I mentor the other developers</span>, build the infrastructure of the React project, and plan/build new features.</li>
+                <li>Part of a team of four that made a new loan approval product. Every commercial loan funnels through this product, and the C-Suite uses it weekly to approve all +$20MM loans through the bank. We <span className="word-highlight">built this in a speedy 4 months</span> with React/OvermindJS on the front end, with a C# .Net backend from start to finish.</li>
+                <li>Built a <span className="word-highlight">new proof of concept in a record 1 week</span> to enable my team lead to successfully make a case for our team to take on a new large Commercial Lending project.</li>
+                <li>I am currently building the frontend of the new Commercial Lending platform with a team of four others. I chose the technologies, planned and built the overall frontend infrastructure, and <span className="word-highlight">mentored other developers</span> to encourage best practices for performance and long-term stability.</li>
             </ul>
         </p>
 
@@ -22,7 +22,7 @@ return (
         <p><span className="word-highlight">Relevant Tech:</span> GatsbyJS, GraphQL, HTML/CSS</p>
         <p>
             <ul>
-                <li>Worked on a team of four people to develop <span className="word-highlight">America's Funniest Videos</span> 30th Anniversary Website.</li>
+                <li>Worked with a team for Vin di Bona Productions to develop the <span className="word-highlight">new America’s Funniest Videos (AFV) website</span> in time for their 30th Anniversary. </li>
             </ul>
         </p>
 
@@ -43,7 +43,28 @@ return (
         <p>
             <ul>
                 <li>Built 3 projects along with a <a href="https://mattwilliamsonportfolio.netlify.app/">Personal Website</a>.</li>
-                <li>Created and deployed mobile-first applications while learning new languages and frameworks by collaborating several hours every week with a senior web developer.</li>
+                <li>Taught coding workshops covering HTML, CSS, jQuery, ES6+, Git / GitHub, APIs, Data Structures.</li>
+                <li>Worked one-on-one with students on code challenges to bridge gaps in understanding.</li>
+            </ul>
+        </p>
+
+        <h3>Franchise Business Consultant @ Mathnasium LLC <span className='smaller'>(May 2011 to Mar 2018)</span></h3>
+        <p>
+            <ul>
+                <li>Guided the growth of Florida from 5 franchise locations in 2011 to 64 franchise locations in 2018.</li>
+                <li>Awarded <span className="word-highlight">Top FBC for 2016</span> due to best revenue growth in the company <span className="word-highlight">(31% increase overall in 2016, 15% same-store sales increase in 2016)</span> along with a 23% increase in 2017.</li>
+                <li>Started as the <span className="word-highlight">first of two Franchise Business Consultants</span> at Mathnasium and helped develop systems to help other team members, including general business Key Performance Indicator systems and Education Key Performance Indicator systems.</li>
+                <li>Developed and led breakout sessions and regional meetings for franchise partners, with topics such as <span className="word-highlight">marketing, lead generation, lead conversion, education, training instructors/staff, controlling expenses</span>, etc. </li>
+            </ul>
+        </p>
+
+        <h3>Owner/Operator @ Mathnasium of New Tampa <span className='smaller'>(Oct 2017 to Aug 2018)</span></h3>
+        <p>
+            <ul>
+                <li>Mathnasium is an international chain of more than 850 learning centers. Since I bought the center in 2007, it consistently achieved <span className="word-highlight">double-digit growth every year</span>.</li>
+                <li>Running the business included marketing, sales, teaching mathematics to students, training instructors, training other franchisees, goal planning, and strategy.</li>
+                <li>Led Mathnasium of New Tampa to be in the <span className="word-highlight">top 75 of Mathnasium centers (out of more than 800)</span>.</li>
+                <li>Personally <span className="word-highlight">trained over 500 instructors</span> to become experts in the Mathnasium method of teaching.</li>
             </ul>
         </p>
     </div>
